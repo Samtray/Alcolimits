500cec58523258b934c9c828279b1fe85889b539
Samtray <85312906+Samtray@users.noreply.github.com>
2021-08-10 15:23:54 -0700

    Admin app view

--- AlcolimitsListViewex/app/src/main/java/mx/edu/uttijuana/alcolimitslistviewex/Drivers.java
+++ AlcolimitsListViewex/app/src/main/java/mx/edu/uttijuana/alcolimitslistviewex/Drivers.java
@@ -16,11 +16,17 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -75,12 +81,41 @@ public class Drivers extends AppCompatActivity {
         client = new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
                         clientId);
 
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if(reconnect){
+                    Log.d("MQTT", "Reconnected");
+                }
+                else{
+                    Log.d("MQTT", "Connected");
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) { Log.d("MQTT", "Connection lost");}
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception { }
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) { Log.d("MQTT", "Message delivered");}
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
         try {
             IMqttToken token = client.connect();
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(true);
+                    client.setBufferOpts(disconnectedBufferOptions);
                     Toast.makeText(getBaseContext(), "MQTT Connected", Toast.LENGTH_SHORT).show();
                 }
 
